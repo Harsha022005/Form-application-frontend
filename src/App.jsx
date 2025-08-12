ec2a1d6994d6f4ab94e2b87f856136f4befd12f9
Your Full Name <harshajami2005@gmail.com>
2025-08-12 18:25:09 +0530

    Changed form.jsx to Form.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Form from "./components/form";
+import Form from "./components/Form";
 import Preview from "./components/PreviewForm";
 
 function App() {
